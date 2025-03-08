74de1fc9276f834b4b96d2352794112d441de70b
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2025-03-08 10:17:29 -0300

    Added main code

--- show_buckets/lib/show_buckets.rb
+++ show_buckets/lib/show_buckets.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 require_relative "show_buckets/version"
+require "aws-sdk-s3"
 
 module ShowBuckets
   class Error < StandardError; end
-  # Your code goes here...
+
+  class BucketLister
+    def initialize
+      @s3 = Aws::S3::Client.new
+    end
+
+    def list_buckets
+      response = @s3.list_buckets
+      response.buckets.map(&:name)
+    rescue Aws::S3::Errors::ServiceError => e
+      puts "Error listing buckets: #{e.message}"
+      []
+    end
+  end
 end
